587ec8fc1858400c1a492445da687bf893fe36c9
佐藤ゆうこ <black.black.enlightment@gmail.com>
2019-05-17 10:50:42 +0800

    初期設定とlayoutファイル作成

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DiaryController extends Controller
+{
+    public function index(){
+    	// dd('Hello Laravel'); ←確認のために使う
+    	// dump and die関数というLaravelに用意された関数
+    	// var_dumpとdieを組み合わせたもの
+    	// Laravel開発の必須ツールです
+
+    	return view('diaries.index');
+    	// view関数はresources/views/内にあるファイルを取得する関数
+    	// view('ファイル名')もしくは
+    	// view('フォルダ名.ファイル名')のように記述する
+    	// 例) view('welcome')
+    	// 例) view('diaries.edit')
+    	// ※ファイル名は .bladeの前のみ
+
+    }
+    public function create(){
+    	return view('diaries.create');
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<title>@yield('title')</title>
+</head>
+<body>
+	@yield('content')
+	</div>
+</body>
+</html>
